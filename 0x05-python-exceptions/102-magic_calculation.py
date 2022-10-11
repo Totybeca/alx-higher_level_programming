c752920c71ad7c61e5ccd1c29cc455e2d5021616
Totybeca <emeraldtoyin>
2022-10-11 04:39:22 +0100

    Added a file

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -5,8 +5,9 @@ def magic_calculation(a, b):
         try:
             if i > a:
                 raise Exception('Too far')
-            result += a ** b / i
+            else:
+                result += a ** b / i
         except:
-            result = b + a
+            result = a + b
             break
     return result
