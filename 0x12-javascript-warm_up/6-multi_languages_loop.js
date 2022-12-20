a7f9ee9342ee7f587eb675627abc4fb253544fec
Totybeca <emeraldtoyin@gmail.com>
2022-12-20 15:36:53 -0800

    javascript

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+
+	  console.log(line);
+
+}
+
+i#!/usr/bin/node
+
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+
+	  console.log(line);
+
+}
+
+
